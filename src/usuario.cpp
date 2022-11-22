730d40edddf6ddd5cd9bdbb0d697a36986ca2fd3
Ana Vitoria Drumond <64758991+anavdrumond@users.noreply.github.com>
2022-11-22 18:46:55 -0300

    Add files via upload

---
+++ src/usuario.cpp
@@ -0,0 +1,12 @@
+#include "usuario.hpp"
+
+Endereco::Endereco (int _cep, std::string _rua, std::string _bairro, 
+    std::string _cidade, std::string _uf, std::string _pais){
+
+        nome = _nome
+        telefone = _telefone;
+        senha = _senha;
+        email = _email;
+        idUsuario = _idUsuario;
+        moeda = _moeda;
+    };
