d1b91340c4ea33c2887cc0d40c1a9d813961a0d8
toninhobaianor <85882536+toninhobaianor@users.noreply.github.com>
2022-11-30 19:23:17 -0300

    Update rede.cpp

--- src/rede.cpp
+++ src/rede.cpp
@@ -12,19 +12,54 @@ void Rede::cancela_usuario(Usuario us){
 
 }
 void Rede::fazer_postagem(Usuario us){
-    std::string apelido_us;
-    std::string descricao_post;
-    float valor_livro;
+    std::cout<<"queremos saber mais sobre o livro"<<std::endl;
+    std::cout<<"qual o nome do livro ?"<<std::endl;
+    std::string obra_l;
+    std::cin>>obra_l;
 
+    std::cout<<"qual o autor ou autora ?"<<std::endl;
+    std::string autor_l;
+    std::cin>>autor_l
+
+    std::cout<<"qual a editora ?"<<std::endl;
+    std::string editora_l;
+    std::cin>>editora_l
+
+    std::cout<<"o livro é novo ou usado ?"<<std::endl;
+    std::string condicao_l;
+    std::cin>>condicao_l;
+
+    std::cout<<"qual ano de lançamento ?"<<std::endl;
+    int ano_l;
+    std::cin>>ano_l
+
+    std::string apelido_us = us.get_idusuario();
+    std::cout<<"de uma descrição:"<<std::endl;
+    std::string descricao_post;
     std::cin>>descricao_post;
+
+    std::cout<<"qual o preço do livro?"<<std::endl;
+    float valor_livro;
     std::cin>>valor_livro;
 
-    Livro liv = Livro();
-    Postagem posta = Postagem(apelido_us, descricao_post, valor_livro);
+    std::cout<<"qual a categoria do livro?"<<std::endl;
+    std::string _categ;
+    std::cin>>_categ;
+    Livro liv = Livro(obra_l,autor_l,editora_l,condicao_l,ano_l);
+    Postagem posta = Postagem(apelido_us, descricao_post, valor_livro,_categ);
 }
-Postagem Rede::pesquisa_postagem(){}
-Usuario Rede::pesquisa_usuario(){}
-void Rede::Redecompra_moeda(){}
+
+Postagem Rede::pesquisa_postagem(){
+
+}
+Usuario Rede::pesquisa_usuario(){
+
+}
+
+void Rede::compra_moeda(Usuario us){
+    char esc = 'c';
+    us.modifica_carteira(esc);
+}
 void Rede::adiciona_amigo(){}
     
 
@@ -43,7 +78,4 @@ Humanidades e Ciências Sociais
 Paternidade e família
 Tecnologia e Ciência
 Outros
-
-
-
-*/
+*
