2a422260f2e2b74a95dcc0af68f1dd8c089d11c1
toninhobaianor <85882536+toninhobaianor@users.noreply.github.com>
2022-11-30 19:12:55 -0300

    Update postagem.cpp

--- src/postagem.cpp
+++ src/postagem.cpp
@@ -1,22 +1,17 @@
 #include <iostream>
-#include <random>
-#include <cstdlib>
-#include <ctime>
 #include "postagem.hpp"
 
-Postagem::Postagem(std::string apelido_, std::string descricao_, float preco_){
+Postagem::Postagem(std::string apelido_, std::string descricao_, float preco_,std::string _cat){
     apelido = apelido_;
     descricao = descricao_;
     preco = preco_;
+    categoria = _cat;
 }
 
 void Postagem::set_livro(Livro l){
     produto = Livro(l.get_obra(), l.get_autor(), l.get_editora(), l.get_condicao(), l.get_tipo(), l.get_ano());
 }
 
-void Postagem::gera_codigo(){
-    
-    unsigned seed = time(0);
-    srand(seed);
-    codigo = 100 + std::rand()%999999;
+std::string Postagem::get_categoria(){
+    return categoria;
 }
