f5c62a1ec923e5043ba5d7b50fe1b5aa42b00a7c
toninhobaianor <85882536+toninhobaianor@users.noreply.github.com>
2022-11-30 17:35:34 -0300

    Update transacao.cpp

--- src/transacao.cpp
+++ src/transacao.cpp
@@ -4,16 +4,16 @@
 #include "venda.hpp"
 #include "troca.hpp"
 
-void Transacao::tipo_acordo(char opcao){
+void Transacao::tipo_acordo(char opcao,Usuario perf){
     if(opcao == 'v'){
-        Venda v;
+        Venda v = Venda();
+        v.operacao(perf);
+        v.modifica_status();
     }
 
     if(opcao == 't'){
-        Troca t;
+        Troca t = Troca();
+        t.operacao(perf);
+        t.modifica_status();
     }
 };
-
-
-
-//Pensar melhor no status !!!!!!!!!!
