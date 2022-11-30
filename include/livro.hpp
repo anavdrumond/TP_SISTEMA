e572f84f6d243aea63c0d7b4db99dad3b06e2f92
toninhobaianor <85882536+toninhobaianor@users.noreply.github.com>
2022-11-30 19:21:08 -0300

    Update livro.hpp

--- include/livro.hpp
+++ include/livro.hpp
@@ -19,17 +19,15 @@ class Livro{
     std::string autor;
     std::string editora;
     std::string condicao;
-    std::string tipo;
     int ano;
 
     public:
     Livro() = default;
-    Livro(std::string obra_, std::string autor_, std::string editora_, std::string condicao_, std::string tipo_, int ano_);   
+    Livro(std::string obra_, std::string autor_, std::string editora_, std::string condicao_,int ano_);   
     std::string get_obra();
     std::string get_autor();
     std::string get_editora();
     std::string get_condicao();
-    std::string get_tipo();
     int get_ano();
 };
 
