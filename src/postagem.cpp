72445ce97c7af224bd11b2b5864b3f6bdfafa532
toninhobaianor <85882536+toninhobaianor@users.noreply.github.com>
2022-12-03 15:53:27 -0300

    Update postagem.cpp

--- src/postagem.cpp
+++ src/postagem.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include "postagem.hpp"
 
-Postagem::Postagem(std::string apelido_, std::string descricao_, float preco_,std::string _cat){
+Postagem::Postagem(std::string apelido_, std::string descricao_, float preco_,std::string _cat,std::string name_p){
     apelido = apelido_;
     descricao = descricao_;
     preco = preco_;
     categoria = _cat;
+    nome_post = name_p;
 }
 
 void Postagem::set_livro(Livro l){
@@ -18,6 +19,17 @@ std::string Postagem::get_categoria(){
 
 std::string Postagem::get_nome_post(){
     return nome_post;
+}
+
+std::string Postagem::get_descricao(){
+    return descricao;
+}
+
+Livro Postagem::get_livro(){
+    return produto;
+}
+float Postagem::get_preco(){
+    return preco;
 }
 
 void Postagem::mostra_postagem(){
